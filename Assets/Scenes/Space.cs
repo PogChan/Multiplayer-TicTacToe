dd5f6f73c504bacb27af0f53064c3e00a960b8c3
AllinaChan <43279583+AllinaChan@users.noreply.github.com>
2023-04-04 23:59:44 -0400

    set text

--- Assets/Scenes/Space.cs
+++ Assets/Scenes/Space.cs
@@ -6,8 +6,10 @@ using Mirror;
 
 public class Space : NetworkBehaviour
 {
-    [SyncVar(hook = nameof(SetSpace))] public Button button;
-    [SyncVar(hook = nameof(SetSpace))] public Text buttonText;
+    public Button button;
+    public Text buttonText;
+
+    [SyncVar(hook = nameof(SetSpace))] private string buttText;
     private GameController gameController;
 
     public void SetControllerReference(GameController control)
@@ -21,13 +23,13 @@ public class Space : NetworkBehaviour
     public void CmdOnCellClick()
     {
         Debug.Log("Just clicked on Cell " + button.name);
-        this.SetSpace();
+        this.SetSpace(this.buttonText.text, gameController.GetSide());
 
         gameController.EndTurn();
     }
-    public void SetSpace()
+    public void SetSpace(string oldValue, string newValue)
     {
-        buttonText.text = gameController.GetSide();
+        buttonText.text = newValue;
         button.interactable = false;
     }
 }
